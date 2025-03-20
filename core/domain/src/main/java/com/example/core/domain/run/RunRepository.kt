3475340792696613c8d09bd5f802ebc0250980f1
unline@linux <esmailalvani.82@gmail.com>
2025-03-20 19:49:07 +0330

    saving pending syncs

--- core/domain/src/main/java/com/example/core/domain/run/RunRepository.kt
+++ core/domain/src/main/java/com/example/core/domain/run/RunRepository.kt
@@ -12,4 +12,5 @@ interface RunRepository {
     suspend fun upsertRun(run: Run, mapPicture: ByteArray): EmptyResult<DataError>
 
     suspend fun deleteRun(id: RunId)
+    suspend fun syncPendingRuns()
 }
