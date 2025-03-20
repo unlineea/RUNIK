f1bb3b24fd76cf776fa65277e003a04aa16f31af
unline@linux <esmailalvani.82@gmail.com>
2025-03-20 20:19:25 +0330

    implementing the sync workManagers

---
+++ run/data/src/main/java/com/example/run/data/di/RunDataModule.kt
@@ -0,0 +1,15 @@
+package com.example.run.data.di
+
+import com.example.run.data.CreateRunWorker
+import com.example.run.data.DeleteRunWorker
+import com.example.run.data.FetchRunsWorker
+import org.koin.androidx.workmanager.dsl.workerOf
+import org.koin.dsl.module
+
+val runDataModule = module {
+    workerOf(::DeleteRunWorker)
+    workerOf(::FetchRunsWorker)
+    workerOf(::CreateRunWorker)
+
+
+}
